dcd3473b84483c1783dcf5ff856eb153ee468faf
reyalexander <alexandercayromamani@gmail.com>
2024-06-24 12:59:25 -0500

    feat: new dist

--- wordcloud/src/router/index.js
+++ wordcloud/src/router/index.js
@@ -10,7 +10,6 @@ const router = createRouter({
     {
       path: "/user",
       name: "user",
-      // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: LoginPanel,
